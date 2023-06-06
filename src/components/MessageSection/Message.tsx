38acca8c33db19972d107f549e73bca06dc46e98
El Mehdi Essaadi <gsio1487@gmail.com>
2023-06-06 15:59:30 +0100

    add date ago and scroll whne message arrived

--- src/components/MessageSection/Message.tsx
+++ src/components/MessageSection/Message.tsx
@@ -1,5 +1,9 @@
 import { useSession } from "next-auth/react";
 import Image from "next/image";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime);
 
 export type MessageType = {
   id: string;
@@ -16,26 +20,24 @@ interface Props {
 
 export const Message = ({ message }: Props) => {
   const { data: session } = useSession();
+  const isCurrentUserMessage = message.name === session?.user?.name;
 
   return (
-    <div
-      className={`flex relative space-x-1 ${
-        message.name === session?.user?.name ? "flex-row-reverse space-x-reverse" : "flex-row"
-      }`}
-    >
-      {message?.avatar && (
-        <div className="w-12 h-12 overflow-hidden flex-shrink-0 rounded">
-          <Image width={50} height={50} src={message.avatar} alt={message.name} />
-        </div>
-      )}
-      <span
-        className={`inline-flex rounded space-x-2 items-start p-3 ${
-          message.name === session?.user?.name ? "bg-[#4a9c6d]" : "bg-[#d275cb]"
-        } `}
-      >
-        {message.name !== session?.user?.name && <span className="font-bold">{message.name}:&nbsp;</span>}
-        {message.body}
-      </span>
+    <div>
+      <div className={`flex relative space-x-1 ${isCurrentUserMessage ? "flex-row-reverse space-x-reverse" : "flex-row"}`}>
+        {message?.avatar && (
+          <div className="w-12 h-12 overflow-hidden flex-shrink-0 rounded">
+            <Image width={50} height={50} src={message.avatar} alt={message.name} />
+          </div>
+        )}
+        <span
+          className={`inline-flex rounded space-x-2 items-start p-3 ${isCurrentUserMessage ? "bg-[#4a9c6d]" : "bg-[#d275cb]"} `}
+        >
+          {message.name !== session?.user?.name && <span className="font-bold">{message.name}:&nbsp;</span>}
+          {message.body}
+        </span>
+      </div>
+      <div className={`text-sm ${isCurrentUserMessage ? "text-right" : ""}`}>{dayjs(message.createdAt).fromNow()}</div>
     </div>
   );
 };
--- src/components/MessageSection/index.tsx
+++ src/components/MessageSection/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { Message, type MessageType } from "./Message";
 import { gql, useQuery } from "@apollo/client";
 import { useSession } from "next-auth/react";
@@ -25,6 +25,8 @@ const GetRecentMessagesQuery = gql`
 `;
 
 function MessageList() {
+  const listElementRef = React.useRef<HTMLDivElement>(null);
+
   const { loading, error, data } = useQuery<{
     messageCollection: { edges: { node: MessageType }[] };
   }>(GetRecentMessagesQuery, {
@@ -33,12 +35,18 @@ function MessageList() {
     },
   });
 
+  useLayoutEffect(() => {
+    if (!data?.messageCollection?.edges?.length) return;
+
+    listElementRef.current?.scrollBy(0, listElementRef.current?.scrollHeight);
+  }, [data?.messageCollection?.edges?.length]);
+
   if (loading) return <p className="text-center">Fetching most recent chat messages.</p>;
 
   if (error) return <p className="text-center">Something went wrong. Refresh to try again.</p>;
 
   return (
-    <div className="h-[max(60vh,300px)] flex flex-col space-y-3 overflow-y-scroll">
+    <div className="h-[max(60vh,300px)] flex flex-col space-y-3 overflow-y-scroll" ref={listElementRef}>
       {data?.messageCollection?.edges?.map(({ node }) => (
         <Message key={node?.id} message={node} />
       ))}
