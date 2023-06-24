b559476f7f6c7fdac66ce81c9a7223df76ef212b
El Mehdi Essaadi <gsio1487@gmail.com>
2023-06-24 22:19:55 +0100

    fix ts error

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,3 @@
-export const GLOBAL_ROOM_ID = process.env.NEXT_PUBLIC_GLOBAL_ROOM_ID;
+export const GLOBAL_ROOM_ID = process.env.NEXT_PUBLIC_GLOBAL_ROOM_ID ?? "";
 export const PAGES_RESTRICTED_FROM_LOGEDIN_USERS = ["/login", "/register"];
 export const PAGES_RESTRICTED_FROM_LOGGEDOUT_USERS = ["/"];
-  
